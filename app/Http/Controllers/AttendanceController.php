9657bb2deacbfca6102bc8c92609cc6165018f48
stdln <alamin.ashik160@gmail.com>
2019-04-23 14:30:08 +0800

    barcode

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -9,19 +9,25 @@ use View;
 class AttendanceController extends Controller
 {
     public function getQRcode( $courseCode, $section){
-        $r=Attendance::where('courseCode',$courseCode)->where('section',$section)->first();
-        $tkey=Str::random(32);
-        $r->key=$tkey;
-        $r->count=0;
-        $r->save();
+        $rr=Attendance::where('courseCode',$courseCode)->where('section',$section)->first();
+        if(!$rr->isEmpty()){
+            $r=$rr->get(0);
+            $tkey=Str::random(32);
+            $r->key=$tkey;
+            $r->count=0;
+            $r->save();
+    
+            $data=array(
+                "courseCode"=>$courseCode,
+                "section"=>$section,
+                "src"=>"https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl=".urlencode($courseCode.';'.$section.";".$tkey)
+            );
+            // echo $data['src'];
+            return View::make("attendance")->with("data",$data);
+        }
+        else
+            return "course code not found";
 
-        $data=array(
-            "courseCode"=>$courseCode,
-            "section"=>$section,
-            "src"=>"https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl=".urlencode($courseCode.';'.$section.";".$tkey)
-        );
-        // echo $data['src'];
-        return View::make("attendance")->with("data",$data);
 
     }
 }

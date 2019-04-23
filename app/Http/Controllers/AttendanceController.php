e125db5dff5af25dd527775adb04339c614c5db7
stdln <alamin.ashik160@gmail.com>
2019-04-23 14:27:25 +0800

    barcode

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -10,15 +10,15 @@ class AttendanceController extends Controller
 {
     public function getQRcode( $courseCode, $section){
         $r=Attendance::where('courseCode',$courseCode)->where('section',$section)->first();
-        $key=Str::random(32);
-        $r->key=$key;
+        $tkey=Str::random(32);
+        $r->key=$tkey;
         $r->count=0;
         $r->save();
 
         $data=array(
             "courseCode"=>$courseCode,
             "section"=>$section,
-            "src"=>"https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl=".urlencode($courseCode.';'.$section.";".$key)
+            "src"=>"https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl=".urlencode($courseCode.';'.$section.";".$tkey)
         );
         // echo $data['src'];
         return View::make("attendance")->with("data",$data);
